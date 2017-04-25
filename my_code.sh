c71e0b5f719498908736bb588512f4ffc61ea482
Sala K405 <salak405@itmsalas.edu.co>
2017-04-24 22:00:13 -0500

    cambios

--- my_code.sh
+++ my_code.sh
@@ -3,3 +3,20 @@ echo "Hello World!"
 =======
 echo "Hello World!"
 >>>>>>> origin
+
+create table EMPLOYEES (
+
+  EMPLOYEE_ID     NUMBER(6) PRIMARY KEY,
+  FIRST_NAME VARCHAR2(20 BYTE),
+  LAST_NAME VARCHAR2(25 BYTE),
+  EMAIL VARCHAR2(25 BYTE),
+  PHONE_NUMBER VARCHAR2(25 BYTE),
+  HIRE_DATE DATE,
+  JOB_ID VARCHAR2(10 BYTE),
+  SALARY NUMBER(8,2),
+  COMMISION_PCT NUMBER(2,2),
+  MANAGER_ID NUMBER(6),
+  department_id   NUMBER(4)                
+);
+
+create index employee_index1 on EMPLOYEES (SALARY);
